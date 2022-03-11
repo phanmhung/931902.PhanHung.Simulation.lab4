c3189aa63583b142975de3dfa046c70bca68503f
DELL <DELL@DESKTOP-NIION0N>
2022-03-11 20:54:00 +0700

    Add project files.

---
+++ lab4/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.cbRandom = new System.Windows.Forms.CheckBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.cbRandom);
+            this.splitContainer1.Panel2.Controls.Add(this.btnStop);
+            this.splitContainer1.Panel2.Controls.Add(this.btnStart);
+            this.splitContainer1.Size = new System.Drawing.Size(1112, 546);
+            this.splitContainer1.SplitterDistance = 448;
+            this.splitContainer1.SplitterWidth = 5;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1112, 448);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // cbRandom
+            // 
+            this.cbRandom.AutoSize = true;
+            this.cbRandom.Location = new System.Drawing.Point(353, 41);
+            this.cbRandom.Name = "cbRandom";
+            this.cbRandom.Size = new System.Drawing.Size(124, 21);
+            this.cbRandom.TabIndex = 6;
+            this.cbRandom.Text = "Create random";
+            this.cbRandom.UseVisualStyleBackColor = true;
+            this.cbRandom.CheckedChanged += new System.EventHandler(this.cbRandom_CheckedChanged);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(188, 19);
+            this.btnStop.Margin = new System.Windows.Forms.Padding(4);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(132, 60);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(13, 19);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(132, 60);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1112, 546);
+            this.Controls.Add(this.splitContainer1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox cbRandom;
+    }
+}
---
+++ lab4/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab4
+{
+    public partial class Form1 : Form
+    {
+        private bool isRunning = false;
+        private bool isRandom = false;
+        private bool[,] map;
+        private int cols;
+        private int rows;
+        private Graphics graphic;
+        private int resolution;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void calNextGen()
+        {
+            graphic.Clear(Color.DodgerBlue);
+
+            var newMap = new bool[cols, rows];
+
+            for (int x = 0; x < cols; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    int neighbours = countNeighbours(x, y);
+                    bool livable = map[x, y];
+
+                    //Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+                    if (livable && neighbours < 2)
+                    {
+                        newMap[x, y] = false;
+                    }
+                    
+                    //Any live cell with two or three live neighbours lives on to the next generation.                    
+                    if (livable && (neighbours == 2 || neighbours == 3))
+                    {
+                        newMap[x, y] = true;
+                    }
+
+                    //Any live cell with more than three live neighbours dies, as if by overpopulation.                    
+                    if (livable && neighbours > 3)
+                    {
+                        newMap[x, y] = false;
+                    }
+
+                    //Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+                    if (!livable && neighbours == 3)
+                    {
+                        newMap[x, y] = true;
+                    }
+
+
+                    if (livable)
+                    {
+                        graphic.FillRectangle(Brushes.LightYellow, x * resolution, y * resolution, resolution - 1, resolution - 1);
+                    }
+                    
+                }
+            }
+
+            map = newMap;
+            pictureBox1.Refresh();
+        }
+
+        private int countNeighbours(int x, int y)
+        {
+            int result = 0;
+
+            for (int i = -1; i < 2; i++)
+            {
+                for (int j = -1; j < 2; j++)
+                {
+                    int col = (x + i + cols) % cols;
+                    int row = (y + j + rows) % rows;
+                    bool isSelfChecking = col == x && row == y;
+
+                    if (!isSelfChecking && map[col, row])
+                    {
+                        result++;
+                    }
+                }
+            }
+            return result;
+        }
+
+        private void startSimulation()
+        {
+            if (isRunning) return;
+
+            resolution = 8;
+            rows = pictureBox1.Height / resolution;
+            cols = pictureBox1.Width / resolution;
+            map = new bool[cols, rows];
+
+            Random random = new Random();
+
+            if (isRandom)
+            {
+                for (int x = 0; x < cols; x++)
+                {
+                    for (int y = 0; y < rows; y++)
+                    {
+                        map[x, y] = random.Next(2) == 0;
+                    }
+                }
+            }
+            else
+            {
+                for (int x = 0; x < cols; x++)
+                {
+                    for (int y = 0; y < rows; y++)
+                    {
+                        map[x, y] = false;
+                    }
+                }
+            }
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height); //init pictureBox
+            graphic = Graphics.FromImage(pictureBox1.Image); // init graphic
+            timer1.Start();
+            isRunning = true;
+
+        }
+
+        private void stopSimulation()
+        {
+            if (!isRunning) return;
+            timer1.Stop();
+            isRunning = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            calNextGen();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            startSimulation();
+            cbRandom.Enabled = false;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            stopSimulation();
+            cbRandom.Enabled = true;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!isRunning) return;
+            
+            if (e.Button == MouseButtons.Left)
+            {
+                var x = e.Location.X / resolution;
+                var y = e.Location.Y / resolution;
+                
+                if (validateMousePosition(x, y)) map[x, y] = true;
+            }
+            if (e.Button == MouseButtons.Right)
+            {
+                var x = e.Location.X / resolution;
+                var y = e.Location.Y / resolution;
+                
+                if (validateMousePosition(x, y)) map[x, y] = false;
+            }
+        }
+
+        private bool validateMousePosition(int x, int y)
+        {
+            return x >= 0 && y >= 0 && x < cols && y < rows;
+        }
+
+        private void cbRandom_CheckedChanged(object sender, EventArgs e)
+        {
+            isRandom = !isRandom;
+        }
+    }
+}
